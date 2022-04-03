cd035048dc59e8cfa02466c5a4da529c61f0139b
Braxton Schafer <braxton@cmdcentral.xyz>
2022-04-02 22:35:12 -0500

    spells can import without error now

---
+++ api/Models/Request/ClassSpellRequest.cs
@@ -0,0 +1,7 @@
+namespace api.Models.Request;
+
+public class ClassSpellRequest
+{
+    public string ClassName { get; set; }
+    public int    Level { get; set; }
+}
---
+++ api/Models/Response/ClassSpellResponse.cs
@@ -0,0 +1,8 @@
+namespace api.Models.Response;
+
+public class ClassSpellResponse
+{
+    public string ClassName { get; set; }
+    public int    Level { get; set; }
+
+}
