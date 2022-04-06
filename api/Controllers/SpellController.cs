17ece41c0e902b8d76a18103224e1707ec46abfa
Braxton Schafer <braxton@cmdcentral.xyz>
2022-04-06 00:03:45 -0500

    fix get spell by name

--- api/Controllers/SpellController.cs
+++ api/Controllers/SpellController.cs
@@ -76,7 +76,7 @@ public class SpellController : ControllerBase
     /// <returns>A JSON object representing the given spell, if it exists</returns>
     /// <response code="200">Returns the spell</response>
     /// <response code="404">Spell with given ID not found</response>
-    [HttpGet("{id:int}")]
+    [HttpGet("{name}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<SpellResponse>> GetSpell(string name)
