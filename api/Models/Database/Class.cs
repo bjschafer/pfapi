f077d3c333bafa2bf631bb56e17aa52975f5a3db
Braxton Schafer <braxton@cmdcentral.xyz>
2022-04-30 19:43:33 -0500

    add seed data for spells/day

--- api/Models/Database/Class.cs
+++ api/Models/Database/Class.cs
@@ -8,5 +8,14 @@ public class Class : ISeedable
     public int    Id   { get; set; }
     public string Name { get; set; }
 
-    // public virtual IDictionary<Spell, int> Spells { get; set; }
+    public IDictionary<int, Dictionary<int, int>>? SpellsPerDay { get; set; }
+
+    public int? MaxSpellLevel(int? characterLevel)
+    {
+        if (characterLevel is not null && SpellsPerDay is not null)
+        {
+            return SpellsPerDay[characterLevel.Value].Max().Key;
+        }
+        return null;
+    }
 }
